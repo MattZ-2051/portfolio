35063478f581ed1dd7841956561efd4f9417c935
MattZ-2051 <66799749+MattZ-2051@users.noreply.github.com>
2025-01-22 23:50:20 +0000

    Deploying to gh-pages from @ MattZ-2051/portfolio@bec80f1221bd3397ac4f879d5fb80c4e6aeac841 🚀

---
+++ _app/immutable/nodes/3.c658a6a1.js
@@ -0,0 +1,10 @@
+import{s as i,d as r}from"../chunks/scheduler.7db16a67.js";import{S as l,i as c,r as m,u as d,v as f,d as u,t as p,w as g,g as h,h as v,x as y,k as _,a as $,f as w}from"../chunks/index.1ee87b10.js";import{C as I}from"../chunks/CommonPage.355d8db7.js";function x(o){let e,n=`<div class="w-full flex justify-center"></div> <div class="mt-10 leading-normal"><p>My passion for software development started in college when I picked up a Computer Science
+				minor and began courses. As time passed I felt that the curriculum was not preparing me
+				exactly for what I wanted to do. I wanted to learn to build applications and the
+				interworkings of the internet. Due to Covid classes went online and I felt my education
+				suffered because of it, due to this I decided to pursue full stack development on my own and
+				decided to attend a coding bootcamp and online courses instead.</p> <p class="mt-5">In my free time I enjoy a variety of different activites. Besides programming, I am a big
+				fitness junkie. I love lifting at the gym and also practicing the Martial Art of Jiu Jitzu
+				everyday, competing at several local tournaments trying to improve everyday much like my
+				engineering skills. I also enjoy reading, specifically about macro economics, and
+				philosophy.</p></div>`;return{c(){e=h("div"),e.innerHTML=n,this.h()},l(t){e=v(t,"DIV",{class:!0,"data-svelte-h":!0}),y(e)!=="svelte-1w5fd2f"&&(e.innerHTML=n),this.h()},h(){_(e,"class","flex flex-col w-full h-full justify-center")},m(t,a){$(t,e,a)},p:r,d(t){t&&w(e)}}}function b(o){let e,n;return e=new I({props:{title:"About",$$slots:{default:[x]},$$scope:{ctx:o}}}),{c(){m(e.$$.fragment)},l(t){d(e.$$.fragment,t)},m(t,a){f(e,t,a),n=!0},p(t,[a]){const s={};a&1&&(s.$$scope={dirty:a,ctx:t}),e.$set(s)},i(t){n||(u(e.$$.fragment,t),n=!0)},o(t){p(e.$$.fragment,t),n=!1},d(t){g(e,t)}}}class M extends l{constructor(e){super(),c(this,e,null,b,i,{})}}export{M as component};
