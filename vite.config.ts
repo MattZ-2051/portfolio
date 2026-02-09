20582ee82739286dfab71e87f7d8e1895118f36b
Matt Zamora <66799749+MattZ-2051@users.noreply.github.com>
2026-02-09 13:31:27 -0700

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  base: "/z-portfolio/",
+  base: "/",
   plugins: [react(), mode === "development" && componentTagger()].filter(
     Boolean,
   ),
